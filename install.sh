dbed6f439f3ab8bb3b68ef1f72b61646e5103a2a
Carlos <48597373+Carliquiss@users.noreply.github.com>
2020-04-29 15:37:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#All the configuration is from NovaSpirit Tech (you can see the video here: https://www.youtube.com/watch?v=XCdl5QRyj6E)
+#adapted to the raspberry pi
+
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 #Install grafana
@@ -7,10 +10,17 @@ sudo dpkg -i wget grafana_6.7.3_armhf.deb
 #Install InfluxDB
 sudo apt install influxdb influxdb-client -y
 
-#Init services
-sudo systemctl start grafana-server.service
+#Init services (To connect to grafana just go to localhost:3000 on the web browser with admin:admin as user:password)
+sudo systemctl start grafana-server.service 
 sudo systemctl start influxdb
 
 
 #Install collectd (to get system information as disk space, cpu info, etc) to send information to influx
 sudo apt install collectd
+
+#Edit the file /etc/influxdb/influxdb.conf (or just copy the file named influxdb.conf from this repository) 
+#Edit the file /etc/collectd/collectd.conf (or just copy the file named collectd.conf from this repository) 
+
+#Restart the services
+sudo systemctl restart collectd.service
+sudo systemctl restart influxdb.service
